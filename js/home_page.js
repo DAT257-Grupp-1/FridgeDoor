0c19df0b6577ddfb8a338150a859aca97e9394bf
Linuslin02 <linuslin@student.chalmers.se>
2024-09-18 14:28:07 +0200

    delete text when add is used

--- js/home_page.js
+++ js/home_page.js
@@ -1,6 +1,8 @@
 let ingredient = []
 
 function add_ingredient() {
-    text = document.getElementById("input_field").value;
+    let text = document.getElementById("input_field").value;
+    document.getElementById("input_field").value = "";
     ingredient.push(text);
+
 }
