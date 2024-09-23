490958bf4afd3652c74f6ac84bfebf11e3d14760
Sixten <60339623+Sixtten610@users.noreply.github.com>
2024-09-23 14:02:09 +0200

    Found Title

--- web_scraper/web_scraper.py
+++ web_scraper/web_scraper.py
@@ -5,13 +5,26 @@ from bs4 import BeautifulSoup
 link = "https://www.ica.se/recept/ugnspannkaka-grundrecept-720978/"
 
 #  Defining main function
-def main():
-    driver = webdriver.Chrome()
-    print("hello")
-    driver.get(link)
+def main(link):
 
+    options = webdriver.ChromeOptions()
+    driver = webdriver.Chrome(options=options)
+    options.add_argument("--disable-search-engine-choice-screen")
+    options.add_argument("--headless=new")
+    
+    driver = webdriver.Chrome(options)
+    web_page = link
+    driver.get(web_page)
 
-    driver.close() 
+    title = driver.find_element(By.CLASS_NAME, 'recipe-header__title').get_attribute("innerHTML")
+    print(title)
+    
+
+def print_html(elem):
+    content = elem.get_attribute("innerHTML")
+    soup = BeautifulSoup(content, 'html.parser')
+    pretty_html = soup.prettify()
+    print(pretty_html)
 
 # Program
-main()
+main(link)
